595f8a51b81fbb592378c1586e63dc1017f58a56
nowan02 <starkgeri@gmail.com>
2021-10-27 11:15:03 +0200

    Added JsonObject class for desererialisation, and a simple caching function. The deserialiser is not working, because it doesn't return any value

--- Startup.cs
+++ Startup.cs
@@ -12,12 +12,35 @@ using Microsoft.Extensions.Hosting;
 
 namespace no_API
 {
+    public class JsonObject 
+    {
+        public string Taskbody;
+        public string Upload;
+        public static List<JsonObject> Cache = new List<JsonObject>();
+    }
+
     public class Startup
     {
+        public static void AddToCache(JsonObject Json)
+        {
+            JsonObject.Cache.Capacity = 5;
+            foreach(JsonObject J in JsonObject.Cache)
+            {
+                if(J.Equals(Json))
+                {
+                    Console.WriteLine("Nothing replaced in cache, continue.");
+                    return;
+                }
+            }
+            JsonObject.Cache.Insert(0, Json);
+            JsonObject.Cache.TrimExcess();
+            Console.WriteLine("Cache updated.");
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,9 +74,12 @@ namespace no_API
                     }
 
                     Task Writer = Body.ContinueWith(async Writing => {
-                        using(StreamWriter sw = new StreamWriter($"uploads/{FileName}"))
+                        using(StreamWriter sw = new StreamWriter($"uploads/{FileName}.json"))
                         {
-                            _ = sw.WriteLineAsync($"{await Body}");
+                            _ = sw.WriteLineAsync("{\n" + 
+                                                  "\"Taskbody\":" + $"\"{await Body}\",\n" +
+                                                  "\"Upload\":" + $"\"{DateTime.Now}\"\n" +
+                                                  "}");
                         }
                     });   
                 });
@@ -63,10 +89,29 @@ namespace no_API
                     Task<string> Contains;
                     try
                     {
-                        using(StreamReader sr = new ($"uploads/{FileName}"))
+                        using(StreamReader sr = new ($"uploads/{FileName}.json"))
                         {
                             Contains = sr.ReadToEndAsync();
-                            _ = context.Response.WriteAsync($"The file '{FileName}' Contains the following:\n{await Contains}\n");
+                            _ = context.Response.WriteAsJsonAsync($"{await Contains}");
+                        }
+                    }
+                    catch
+                    {
+                       _ = context.Response.WriteAsync($"No file exists with name '{FileName}'");
+                    }
+                });
+
+                endpoints.MapGet("/api/tasks/{file:required}/{key:required}", async context => {
+                    var FileName = context.Request.RouteValues["file"];
+                    var searchKey = context.Request.RouteValues["key"];
+                    try
+                    {
+                        using(StreamReader sr = new ($"uploads/{FileName}.json"))
+                        {
+                            string Contains = await sr.ReadToEndAsync();
+                            JsonObject Deserialised = JsonSerializer.Deserialize<JsonObject>(Contains);
+                            _ = context.Response.WriteAsync($"{Deserialised.Taskbody} {Deserialised.Upload}");
+                            AddToCache(Deserialised);
                         }
                     }
                     catch
@@ -77,7 +122,7 @@ namespace no_API
 
                 endpoints.MapDelete("/api/tasks/{file:required}", async context => {
                     var FileName = context.Request.RouteValues["file"];
-                    _ = context.Response.WriteAsync($"The file '{FileName}' will be deleted");
+                    _ = context.Response.WriteAsync($"Trying to delete '{FileName}'...");
                     try
                     {
                         File.Delete($"uploads/{FileName}");
